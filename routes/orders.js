cad8f53ac5c0253cc0844751bbb72bf05f4e22c8
jonniek01 <jontejonhn@gmail.com>
2023-01-31 14:30:22 +0300

    presentations

---
+++ routes/orders.js
@@ -0,0 +1,20 @@
+//orders route
+const express = require('express');
+const orders = express.Router();
+const  {makeOrder, getOrders} = require ('../controllers/orders');
+// const {obtainAccessToken, mpesaExpressInt, confirmation} = require('../middleware/mpesa');
+
+
+//route to get the auth token
+// orders.get('/get-auth-token',obtainAccessToken);
+
+//lipa na mpesa online 
+orders.post('/makeorder', makeOrder);
+
+//callback url
+orders.post('/mpesa/confirmation',confirmation)
+
+orders.get('/get/all',getOrders)
+
+
+module.exports = {orders};
